10a72b1cf15b4c03e8faac9821d7066efba44684
Adrien BAK <adrien.bak@metascale.org>
2014-10-20 10:18:06 +0900

    piece scaffold

---
+++ piece.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+
+class Piece:
+  def __init__(self, list):
+    self._internal_array = np.zeros(9,9)
+    #the hole
+    self._internal_array[4,4] = 1
+    for(y, x) in list:
+      self._internal_array[y, x] = 1

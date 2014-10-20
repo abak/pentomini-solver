6b0e9808fe95f6a87a3afe2619cfcd222db81ac8
Adrien BAK <adrien.bak@gmail.com>
2014-10-20 11:11:19 +0900

    fix error

--- piece.py
+++ piece.py
@@ -3,8 +3,11 @@ import numpy as np
 
 class Piece:
   def __init__(self, list):
-    self._internal_array = np.zeros(9,9)
+    self._internal_array = np.zeros((9,9))
     #the hole
     self._internal_array[4,4] = 1
     for(y, x) in list:
       self._internal_array[y, x] = 1
+
+  def pretty_print(self):
+    print str(self._internal_array)

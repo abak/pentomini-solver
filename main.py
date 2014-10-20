91509fac9c9568f4dd22c322e2b270fb9ee3a64e
Adrien BAK <adrien.bak@gmail.com>
2014-10-20 12:40:22 +0900

    refactor piece

--- main.py
+++ main.py
@@ -4,31 +4,25 @@ from piece import Piece
 
 def init_pieces():
   res = []
-  res.append(Piece([(4, 3), (4, 5), (4, 6), (5, 5)]))
-  res.append(Piece([(4, 3), (5, 3), (6, 3), (4, 5)]))
-  res.append(Piece([(4, 3), (4, 5), (4, 6), (4, 7)]))
-  res.append(Piece([(5, 3), (5, 4), (5, 5), (6, 4)]))
-  res.append(Piece([(5, 4), (4, 5), (4, 6), (5, 6)]))
-  res.append(Piece([(4, 3), (5, 3), (4, 5), (4, 6)]))
-  res.append(Piece([(5, 4), (5, 5), (5, 6), (6, 5)]))
-  res.append(Piece([(5, 4), (6, 4), (5, 5), (6, 5)]))
-  res.append(Piece([(4, 3), (5, 3), (5, 2), (6, 2)]))
-  res.append(Piece([(4, 5), (5, 5), (6, 5), (6, 6)]))
-  res.append(Piece([(5, 4), (5, 3), (6, 3), (7, 3)]))
-  res.append(Piece([(4, 5), (4, 6), (5, 5), (6, 5)]))
+  res.append(Piece([(0, -1), (0, 1), (0, 2), (1, 1)]))
+  res.append(Piece([(0, -1), (1, -1), (2, -1), (0, 1)]))
+  res.append(Piece([(0, -1), (0, 1), (0, 2), (0, 3)]))
+  res.append(Piece([(1, -1), (1, 0), (1, 1), (2, 0)]))
+  res.append(Piece([(1, 0), (0, 1), (0, 2), (1, 2)]))
+  res.append(Piece([(0, -1), (1, -1), (0, 1), (0, 2)]))
+  res.append(Piece([(1, 0), (1, 1), (1, 2), (2, 1)]))
+  res.append(Piece([(1, 0), (2, 0), (1, 1), (2, 1)]))
+  res.append(Piece([(0, -1), (1, -1), (1, -2), (2, -2)]))
+  res.append(Piece([(0, 1), (1, 1), (2, 1), (2, 2)]))
+  res.append(Piece([(1, 0), (1, -1), (2, -1), (3, -1)]))
+  res.append(Piece([(0, 1), (0, 2), (1, 1), (2, 1)]))
   return res
 
 def rotate_piece(piece, orientation):
   #probably highly ineffective
   new_piece = Piece()
-  new_piece._internal_array = np.copy(piece._internal_array)
-
-  if orientation >= 4:
-    print "coucou"
-    new_piece._internal_array = np.fliplr(new_piece._internal_array)
-
-  new_piece._internal_array = np.rot90(new_piece._internal_array, orientation%4)
-
+  new_piece._list = list(piece._list)
+  new_piece.rotate(orientation)
   return new_piece
 
 def is_move_valid(board, piece, nail, orientation):
@@ -39,15 +33,15 @@ def main():
   board = Board()
   pieces = init_pieces()
 
-  # for p in pieces:
-  #   p.pretty_print()
-  #   print
+  for p in pieces:
+    p.pretty_print()
+    print
 
-  p = pieces[0]
+  # p = pieces[0]
 
-  for i in range (0, 8):
-    toto = rotate_piece(p, i)
-    toto.pretty_print()
+  # for i in range (0, 8):
+  #   toto = rotate_piece(p, i)
+  #   toto.pretty_print()
 
 if __name__ == '__main__':
   main()
--- piece.py
+++ piece.py
@@ -2,12 +2,36 @@ import numpy as np
 
 
 class Piece:
-  def __init__(self, list):
-    self._internal_array = np.zeros((9,9))
-    #the hole
-    self._internal_array[4,4] = 1
-    for(y, x) in list:
-      self._internal_array[y, x] = 1
+  def __init__(self, list=[]):
+    self._list = list
 
   def pretty_print(self):
-    print str(self._internal_array)
+    tmp = np.zeros((9, 9))
+    tmp[4, 4] = 1
+    for (y, x) in self._list:
+      tmp[y+4, x+4] = 1
+    print str(tmp) + "\n"
+
+  def rotate(self, orientation):
+    if orientation >= 4:
+      self._list = [(y, -x) for y, x in self._list]
+
+    if orientation %4 == 0:
+      return
+    elif orientation %4 == 1:
+      self._list = [(-x, y) for y,x in self._list]
+    elif orientation %4 == 2:
+      self._list = [(-y, -x) for y, x in self._list]
+    elif orientation %4 == 3:
+      self._list = [(x, -y) for y,x in self._list]
+
+
+if __name__ == '__main__':
+  p = Piece([(0, -1), (0, 1), (0, 2), (1, 1)])
+  p.pretty_print()
+
+  for i in range(0, 8):
+    print i
+    p = Piece([(0, -1), (0, 1), (0, 2), (1, 1)])
+    p.rotate(i)
+    p.pretty_print()

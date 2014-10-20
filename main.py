b3ff2ce53069d69a30ddd0e88f2ad3b46170ed55
Adrien BAK <adrien.bak@gmail.com>
2014-10-20 12:14:21 +0900

    add piece rotation

--- main.py
+++ main.py
@@ -18,14 +18,36 @@ def init_pieces():
   res.append(Piece([(4, 5), (4, 6), (5, 5), (6, 5)]))
   return res
 
+def rotate_piece(piece, orientation):
+  #probably highly ineffective
+  new_piece = Piece()
+  new_piece._internal_array = np.copy(piece._internal_array)
+
+  if orientation >= 4:
+    print "coucou"
+    new_piece._internal_array = np.fliplr(new_piece._internal_array)
+
+  new_piece._internal_array = np.rot90(new_piece._internal_array, orientation%4)
+
+  return new_piece
+
+def is_move_valid(board, piece, nail, orientation):
+  piece_to_put = rotate_piece(piece, orientation)
+
 
 def main():
   board = Board()
   pieces = init_pieces()
 
-  for p in pieces:
-    p.pretty_print()
-    print
+  # for p in pieces:
+  #   p.pretty_print()
+  #   print
+
+  p = pieces[0]
+
+  for i in range (0, 8):
+    toto = rotate_piece(p, i)
+    toto.pretty_print()
 
 if __name__ == '__main__':
   main()
